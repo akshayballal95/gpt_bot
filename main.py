470f75ca065b66c3685d96e8574197c539f54db0
= <=>
2023-06-09 22:33:17 +0530

    langchain

--- main.py
+++ main.py
@@ -1,27 +1,44 @@
 import logging
 from telegram import Update
 from telegram.ext import filters, ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler
-import openai
 from dotenv import load_dotenv
 import os
 
-load_dotenv('key.env')
-openai.api_key = os.getenv('OPENAPI_KEY')
+from langchain import OpenAI, ConversationChain, LLMChain, PromptTemplate
+from langchain.memory import ConversationBufferWindowMemory
+
+
+load_dotenv()
+
+# openai.api_key = os.getenv('OPENAPI_API_KEY')
 token = os.environ.get('TOKEN')
 
-async def gpt_response(prompt):
-  response = openai.Completion.create(
-  engine="text-davinci-003",
-  prompt=prompt,
-  temperature=0.5,
-  max_tokens = 256, 
-  top_p=1.0,
-  frequency_penalty=0.0,
-  presence_penalty=0.0,
 
+
+template = """Assistant is a large language model trained by OpenAI.
+
+Assistant is designed to be able to assist with a wide range of tasks, from answering simple questions to providing in-depth explanations and discussions on a wide range of topics. As a language model, Assistant is able to generate human-like text based on the input it receives, allowing it to engage in natural-sounding conversations and provide responses that are coherent and relevant to the topic at hand.
+
+Assistant is constantly learning and improving, and its capabilities are constantly evolving. It is able to process and understand large amounts of text, and can use this knowledge to provide accurate and informative responses to a wide range of questions. Additionally, Assistant is able to generate its own text based on the input it receives, allowing it to engage in discussions and provide explanations and descriptions on a wide range of topics.
+
+Overall, Assistant is a powerful tool that can help with a wide range of tasks and provide valuable insights and information on a wide range of topics. Whether you need help with a specific question or just want to have a conversation about a particular topic, Assistant is here to assist.
+
+{history}
+Human: {human_input}
+Assistant:"""
+
+prompt = PromptTemplate(
+    input_variables=["history", "human_input"], 
+    template=template
 )
-  return response['choices'][0]['text']
 
+
+chatgpt_chain = LLMChain(
+    llm=OpenAI(temperature=0.3, max_tokens=512), 
+    prompt=prompt, 
+    verbose=True, 
+    memory=ConversationBufferWindowMemory(k=2),
+)
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -29,8 +46,8 @@ logging.basicConfig(
 )
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    text = await gpt_response(update.message.text)
-
+    text = chatgpt_chain.predict(human_input=update.message.text)
+    # text = await gpt_response(update.message.text)
     await context.bot.send_message(chat_id=update.effective_chat.id, text=text)
 
 if __name__ == '__main__':
